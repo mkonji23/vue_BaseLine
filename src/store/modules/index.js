03efc26511241e6be86396f277978b1866a075a3
min <mkonji23@naver.com>
2020-08-06 14:38:40 +0900

    구조 정리vuex

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -4,12 +4,9 @@ const files = require.context('.', false, /\.js$/);
 const modules = {};
 
 files.keys().forEach(fileName => {
-	console.info('fileName', fileName);
 	if (fileName === './index.js') return;
 	const moduleName = camelCase(fileName.replace(/(\.\/|\.js)/g, ''));
 	modules[moduleName] = { namespaced: true, ...files(fileName).default };
-	console.info('moduleName', moduleName);
-	console.info('modules[moduleName]', modules[moduleName]);
 });
 
 export default modules;
